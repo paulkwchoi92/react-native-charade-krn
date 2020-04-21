a87a5ee283f1a2517bab498be840a95640ef790d
paulkwchoi92 <paulkwchoi92@gmail.com>
2020-04-21 10:42:20 -0700

    going into native

--- react-web-starter-master/src/app/Todo.js
+++ react-web-starter-master/src/app/Todo.js
@@ -1,26 +1,46 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export class Todo extends Component {
   constructor() {
     super();
     this.state = {
       todos: [],
-      newTodo: ''
-    }
+      newTodo: "",
+    };
   }
 
+  handleChange(e) {
+    const { value } = e.target;
+    this.setState({ newTodo: value });
+  }
+
+  handleClick(e) {
+    e.preventDefault();
+    const todos = [...this.state.todos, this.input.value];
+    //  above code is short hand for concatting
+    this.input.value = ""
+    this.setState({ todos});
+  }
 
   render() {
     return (
       <div>
         <form>
-          <input value={this.state.newTodo} type="text" placeholder="new todo" />
-          <button>create></button>
+          <input
+            ref={node => this.input = node}
+            // value={this.state.newTodo}
+            type="text"
+            placeholder="new todo"
+            // onChange={this.handleChange.bind(this)}
+          />
+          <button onClick={this.handleClick.bind(this)}>create></button>
         </form>
         <ul>
-          {this.state.todos.map(todo => <li>{todo}</li>)}
+          {this.state.todos.map((todo) => (
+            <li>{todo}</li>
+          ))}
         </ul>
-     </div>
-    )
+      </div>
+    );
   }
 }
