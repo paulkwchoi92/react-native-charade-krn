570b2cb1fa5f6a7fd3eec428f9557b4355670455
paulkwchoi92 <paulkwchoi92@gmail.com>
2020-04-26 22:10:25 -0700

    testing ss code

--- Frontend/src/utils/cards/movie.js
+++ Frontend/src/utils/cards/movie.js
@@ -1,3 +1,39 @@
-export const movieArr = [
-  
-]
+onSubmit = async formValues => {
+  _logger(formValues);
+  let arrayValues;
+  if (formValues.category === '1') {
+    arrayValues = await formValues.orgs.map(
+      org =>
+        (org = {
+          organizationId: parseInt(org),
+          email: formValues.email,
+        }),
+    );
+
+    userOrganizationService.updateAllEmails();
+  } else if (formValues.category === '2') {
+    formValues.orgs.forEach(org => {
+      arrayValues.push({
+        organizationId: parseInt(org),
+        phoneNumber: formValues.phone,
+      });
+    });
+    userOrganizationService.updateAllPhones();
+  } else if (formValues.category === '3') {
+    formValues.orgs.forEach(org => {
+      arrayValues.push({
+        organizationId: parseInt(org),
+        locationId: parseInt(formValues.address),
+      });
+    });
+    userOrganizationService.updateAllLocations();
+  } else if (formValues.category === '4') {
+    formValues.orgs.forEach(org => {
+      arrayValues.push({
+        organizationId: parseInt(org),
+        paymentId: parseInt(formValues.payment),
+      });
+    });
+    userOrganizationService.updateAllPayments();
+  }
+};
